2422eb2df80b6e5530c25f019205fb56d0011c8a
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2011-08-02 12:30:47 -0400

    Added dark mode for graphs

--- src/metricsd/web/file_browser.go
+++ src/metricsd/web/file_browser.go
@@ -2,23 +2,23 @@ package web
 
 import (
 	"io/ioutil"
-    "path"
+	"path"
 	"sort"
-    "strings"
+	"strings"
 	"metricsd/config"
 )
 
 type graphItem struct {
-    Name   string
-    Writer string
-    Group  string
-    Title  string
+	Name	string
+	Writer	string
+	Group	string
+	Title	string
 }
 
 func (graph *graphItem) Less(graphToCompare interface{}) bool {
-    g := graphToCompare.(*graphItem)
-    return graph.Name < g.Name ||
-        (graph.Name == g.Name && graph.Writer < g.Writer)
+	g := graphToCompare.(*graphItem)
+	return graph.Name < g.Name ||
+		(graph.Name == g.Name && graph.Writer < g.Writer)
 }
 
 type graphItemsList []*graphItem
@@ -40,23 +40,23 @@ func (l graphItemsList) Less(i, j int) bool {
 
 
 type graphItemGroup struct {
-    Group    string
-    HasGroup bool
-    Graphs   graphItemsList
+	Group		string
+	HasGroup	bool
+	Graphs		graphItemsList
 }
 
 func (group *graphItemGroup) Less(groupToCompare *graphItemGroup) bool {
-    return len(groupToCompare.Group) == 0 || (len(group.Group) > 0 && group.Group < groupToCompare.Group)
+	return len(groupToCompare.Group) == 0 || (len(group.Group) > 0 && group.Group < groupToCompare.Group)
 }
 
 type graphItemSource struct {
-    Source string
-    Graphs graphItemsList
+	Source	string
+	Graphs	graphItemsList
 }
 
 func (source *graphItemSource) Less(sourceToCompare interface{}) bool {
-    s := sourceToCompare.(*graphItemSource)
-    return source.Source == "all" || (s.Source != "all" && source.Source < s.Source)
+	s := sourceToCompare.(*graphItemSource)
+	return source.Source == "all" || (s.Source != "all" && source.Source < s.Source)
 }
 
 type graphItemGroupsList []*graphItemGroup
@@ -82,81 +82,81 @@ type Browser struct{}
 var browser = &Browser{}
 
 func (browser *Browser) ListCountGraphsGrouped() (groups graphItemGroupsList) {
-    groups = make(graphItemGroupsList, 0, 10)
-    for _, file := range browser.List("all", "", "-count.rrd") {
-        found := false
-        for _, group := range groups {
-            if file.Group == group.Group {
-                group.Graphs = append(group.Graphs, file)
-                found = true
-            }
-        }
-        if !found {
-            group := &graphItemGroup{file.Group, len(file.Group) > 0, make(graphItemsList, 0, 10)}
-            group.Graphs = append(group.Graphs, file)
-            groups = append(groups, group)
-        }
-    }
-    sort.Sort(groups)
-    for _, group := range groups {
-        sort.Sort(group.Graphs)
-    }
+	groups = make(graphItemGroupsList, 0, 10)
+	for _, file := range browser.List("all", "", "-count.rrd") {
+		found := false
+		for _, group := range groups {
+			if file.Group == group.Group {
+				group.Graphs = append(group.Graphs, file)
+				found = true
+			}
+		}
+		if !found {
+			group := &graphItemGroup{file.Group, len(file.Group) > 0, make(graphItemsList, 0, 10)}
+			group.Graphs = append(group.Graphs, file)
+			groups = append(groups, group)
+		}
+	}
+	sort.Sort(groups)
+	for _, group := range groups {
+		sort.Sort(group.Graphs)
+	}
 
-    return
+	return
 }
 
 func (browser *Browser) ListSources(metric string) (sources []*graphItemSource) {
-    sources = make([]*graphItemSource, 0, 10)
-    dir, err := ioutil.ReadDir(path.Join(config.DataDir))
-    if err != nil {
-        return
-    }
-    for _, fi := range dir {
-        if !fi.IsDirectory() {
-            continue
-        }
-        if graphs := browser.List(fi.Name, metric, ".rrd"); len(graphs) > 0 {
-            sources = append(sources, &graphItemSource{fi.Name, graphs})
-        }
-    }
-    return
+	sources = make([]*graphItemSource, 0, 10)
+	dir, err := ioutil.ReadDir(path.Join(config.DataDir))
+	if err != nil {
+		return
+	}
+	for _, fi := range dir {
+		if !fi.IsDirectory() {
+			continue
+		}
+		if graphs := browser.List(fi.Name, metric, ".rrd"); len(graphs) > 0 {
+			sources = append(sources, &graphItemSource{fi.Name, graphs})
+		}
+	}
+	return
 }
 
 func (*Browser) List(source, metric, suffix string) (files graphItemsList) {
-    files = make(graphItemsList, 0, 10)
-    dir, err := ioutil.ReadDir(path.Join(config.DataDir, source))
-    if err != nil {
-        return
-    }
+	files = make(graphItemsList, 0, 10)
+	dir, err := ioutil.ReadDir(path.Join(config.DataDir, source))
+	if err != nil {
+		return
+	}
 
-    for _, fi := range dir {
-        if fi.IsDirectory() {
-            continue
-        }
+	for _, fi := range dir {
+		if fi.IsDirectory() {
+			continue
+		}
 
-        if strings.HasSuffix(fi.Name, suffix) {
-            var name, writer, group, title string
+		if strings.HasSuffix(fi.Name, suffix) {
+			var name, writer, group, title string
 
-            split := strings.LastIndex(fi.Name, "-")
-            name = fi.Name[:split]
-            if len(metric) > 0 && name != metric {
-                continue
-            }
-            writer = fi.Name[split+1 : len(fi.Name)-len(".rrd")]
+			split := strings.LastIndex(fi.Name, "-")
+			name = fi.Name[:split]
+			if len(metric) > 0 && name != metric {
+				continue
+			}
+			writer = fi.Name[split+1 : len(fi.Name)-len(".rrd")]
 
-            split = strings.Index(name, "$")
+			split = strings.Index(name, "$")
 			if split < 0 {
 				split = strings.Index(name, ".")
 			}
-            if split >= 0 {
-                group = name[:split]
-                title = name[split+1:]
+			if split >= 0 {
+				group = name[:split]
+				title = name[split+1:]
 			} else {
-                group = ""
-                title = name
-            }
-            files = append(files, &graphItem{name, writer, group, title})
-        }
-    }
-    return
+				group = ""
+				title = name
+			}
+			files = append(files, &graphItem{name, writer, group, title})
+		}
+	}
+	return
 }
--- src/metricsd/web/web.go
+++ src/metricsd/web/web.go
@@ -1,132 +1,134 @@
 package web
 
 import (
-    "fmt"
-    "io"
-    "os"
-    "path"
-    "strings"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"strings"
 	"metricsd/config"
-    "github.com/hoisie/web.go"
-    "github.com/hoisie/mustache.go"
+	"github.com/hoisie/web.go"
+	"github.com/hoisie/mustache.go"
 )
 
 /***** Web routines ***********************************************************/
 
 func Start() {
 	web.Config.StaticDir = path.Join(config.RootDir, "public")
-    web.Get("/", summary)
-    web.Get("/metric/(.*)/(.*)/(.*)", metric_graph)
-    web.Get("/metric/(.*)/(.*)", host_metric)
-    web.Get("/metric/(.*)", metric)
-    web.Get("/graph/(.*)/(.*)/(.*)\\.png", graph)
-    web.Get("/graph/(.*)/(.*)/(.*)", graph)
-    web.Get("/host/(.*)", host)
-    web.Run(config.Listen)
+	web.Get("/", summary)
+	web.Get("/metric/(.*)/(.*)/(.*)", metric_graph)
+	web.Get("/metric/(.*)/(.*)", host_metric)
+	web.Get("/metric/(.*)", metric)
+	web.Get("/graph/(.*)/(.*)/(.*)\\.png", graph)
+	web.Get("/graph/(.*)/(.*)/(.*)", graph)
+	web.Get("/host/(.*)", host)
+	web.Run(config.Listen)
 }
 
 func summary() string {
-    return mustache.RenderFile(template("summary"), map[string]interface{}{
-        "metrics": browser.ListCountGraphsGrouped(),
-    })
+	return mustache.RenderFile(template("summary"), map[string]interface{}{
+		"metrics": browser.ListCountGraphsGrouped(),
+	})
 }
 
 func metric(metric string) string {
-    return mustache.RenderFile(template("metric"), map[string]interface{}{
-        "metric": metric,
-        "hosts":  browser.ListSources(metric),
-    })
+	return mustache.RenderFile(template("metric"), map[string]interface{}{
+		"metric": metric,
+		"hosts":  browser.ListSources(metric),
+	})
 }
 
 func host_metric(metric, source string) string {
-    return mustache.RenderFile(template("host_metric"), map[string]interface{}{
-        "source":  source,
-        "metric":  metric,
-        "metrics": browser.List("all", metric, ".rrd"),
-    })
+	return mustache.RenderFile(template("host_metric"), map[string]interface{}{
+		"source":  source,
+		"metric":  metric,
+		"metrics": browser.List("all", metric, ".rrd"),
+	})
 }
 
 func host(source string) string {
-    return mustache.RenderFile(template("host"), map[string]interface{}{
-        "source":  source,
-        "metrics": browser.List(source, "", "-count.rrd"),
-    })
+	return mustache.RenderFile(template("host"), map[string]interface{}{
+		"source":  source,
+		"metrics": browser.List(source, "", "-count.rrd"),
+	})
 }
 
 func metric_graph(metric, source, writer string) string {
-    return mustache.RenderFile(template("metric_graph"), map[string]interface{}{
-        "source": source,
-        "metric": metric,
-        "writer": writer,
-    })
+	return mustache.RenderFile(template("metric_graph"), map[string]interface{}{
+		"source": source,
+		"metric": metric,
+		"writer": writer,
+	})
 }
 
 func graph(ctx *web.Context, source, metric, writer string) {
-    ctx.SetHeader("Content-Type", "image/png", true)
+	ctx.SetHeader("Content-Type", "image/png", true)
 
-    params := struct {
-        Rra           string
-        Width, Height int
-    }{"daily", 620, 240}
-    ctx.Request.UnmarshalParams(&params)
+	params := struct {
+		Rra				string
+		Width, Height	int
+		Dark			bool
+	}{"daily", 620, 240, false}
+	ctx.Request.UnmarshalParams(&params)
 
-    var from int
-    switch params.Rra {
-    case "hourly":
-        from = -14400
-    case "daily":
-        from = -86400
-    case "weekly":
-        from = -604800
-    case "monthly":
-        from = -2678400
-    case "yearly":
-        from = -33053184
-    default:
-        from = -86400
-        params.Rra = "daily"
-    }
+	var from int
+	switch params.Rra {
+	case "hourly":
+		from = -14400
+	case "daily":
+		from = -86400
+	case "weekly":
+		from = -604800
+	case "monthly":
+		from = -2678400
+	case "yearly":
+		from = -33053184
+	default:
+		from = -86400
+		params.Rra = "daily"
+	}
 
-    rrd_file := fmt.Sprintf("%s/%s/%s-%s.rrd", config.DataDir, source, metric, writer)
-    args := mustache.RenderFile(template("writers/"+writer), map[string]interface{}{
-        "source":   source,
-        "metric":   metric,
-        "writer":   writer,
-        "rrd_file": rrd_file,
-        "from":     from,
-        "width":    params.Width,
-        "height":   params.Height,
-        "rra":      params.Rra,
-        "interval": config.SliceInterval,
-    })
-    r, w, err := os.Pipe()
+	rrd_file := fmt.Sprintf("%s/%s/%s-%s.rrd", config.DataDir, source, metric, writer)
+	args := mustache.RenderFile(template("writers/"+writer), map[string]interface{}{
+		"source":	source,
+		"metric":	metric,
+		"writer":	writer,
+		"rrd_file":	rrd_file,
+		"from":		from,
+		"width":	params.Width,
+		"height":	params.Height,
+		"rra":		params.Rra,
+		"interval":	config.SliceInterval,
+		"dark":		params.Dark,
+	})
+	r, w, err := os.Pipe()
 	if err != nil {
-        config.Logger.Error("Pipe: %s", err)
-        return
-    }
+		config.Logger.Error("Pipe: %s", err)
+		return
+	}
 
-    // config.Logger.Debug("started, %s", strings.Split(args, "\n", -1))
-    attr := &os.ProcAttr{Dir: "", Env: os.Environ(), Files: []*os.File{nil, w, w}}
-    process, err := os.StartProcess("/usr/bin/rrdtool", strings.Split(args, "\n"), attr)
-    defer process.Release()
-    w.Close()
-    io.Copy(ctx, r)
-    r.Close()
+	// config.Logger.Debug("started, %s", strings.Split(args, "\n", -1))
+	attr := &os.ProcAttr{Dir: "", Env: os.Environ(), Files: []*os.File{nil, w, w}}
+	process, err := os.StartProcess("/usr/bin/rrdtool", strings.Split(args, "\n"), attr)
+	defer process.Release()
+	w.Close()
+	io.Copy(ctx, r)
+	r.Close()
 
-    wait, err := process.Wait(0)
-    if err != nil {
-        config.Logger.Error("wait: %s\n", err)
-        return
-    }
-    if !wait.Exited() || wait.ExitStatus() != 0 {
-        config.Logger.Error("date: %v\n", wait)
-        return
-    }
-    return
+	wait, err := process.Wait(0)
+	if err != nil {
+		config.Logger.Error("wait: %s\n", err)
+		return
+	}
+	if !wait.Exited() || wait.ExitStatus() != 0 {
+		config.Logger.Error("date: %v\n", wait)
+		return
+	}
+	return
 }
 
 /***** Helper functions *******************************************************/
 
 func template(name string) string {
-    return path.Join(config.RootDir, fmt.Sprintf("templates/%s.mustache", name))
+	return path.Join(config.RootDir, fmt.Sprintf("templates/%s.mustache", name))
 }
